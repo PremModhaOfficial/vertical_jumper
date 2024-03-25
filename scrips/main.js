4e609c2cf7d1cc631ddf4c93c56eaf8a584574a4
prem <premmodhaofficial@gmail.com>
2024-03-25 09:52:05 +0530

    init

---
+++ scrips/main.js
@@ -0,0 +1,18 @@
+
+canvas = document.getElemnt
+
+// Get the 2D context
+ctx = canvas.getContext("2d");
+
+
+for (var i = 0; i < 100; i++) {
+    ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+    ctx.fillRect(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 10, 10);
+  console.log("i: " + i);
+}
+
+function draw() {
+    ctx.fillStyle = "rgb(" + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + "," + Math.floor(Math.random() * 256) + ")";
+    ctx.fillRect(Math.floor(Math.random() * canvas.width), Math.floor(Math.random() * canvas.height), 10, 10);
+    console.log("draw");
+ }
